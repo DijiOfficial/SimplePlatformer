58bb542cd72e626df0470f1f1c528accb0c707eb
Dylan Burgisser <dylan.burgisser@student.howest.be>
2025-10-16 23:58:30 +0100

    feat: added AutoDestroy component implementation
    Implements the AutoDestroy component, which schedules the destruction of its owner GameObject after a specified lifetime using the TimerManager and SceneManager singletons.

---
+++ Engine/Engine/Components/AutoDestroy.cpp
@@ -0,0 +1,13 @@
+﻿#include "AutoDestroy.h"
+
+#include "../Singleton/SceneManager.h"
+#include "../Singleton/TimerManager.h"
+#include "../Core/GameObject.h"
+
+void diji::AutoDestroy::Init()
+{
+    (void)TimerManager::GetInstance().SetTimer([&]
+    {
+        SceneManager::GetInstance().SetPendingDestroy(GetOwner());
+    }, m_LifeTime, false);
+}
---
+++ Engine/Engine/Components/AutoDestroy.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "../Components/Component.h"
+
+namespace diji
+{
+    class AutoDestroy final : public Component
+    {
+    public:
+        explicit AutoDestroy(GameObject* ownerPtr, const float lifeTime) : Component{ ownerPtr }, m_LifeTime{ lifeTime } {}
+        ~AutoDestroy() noexcept override = default;
+
+        AutoDestroy(const AutoDestroy& other) = delete;
+        AutoDestroy(AutoDestroy&& other) = delete;
+        AutoDestroy& operator=(const AutoDestroy& other) = delete;
+        AutoDestroy& operator=(AutoDestroy&& other) = delete;
+
+        void Init() override;
+        void OnEnable() override {}
+        void Start() override {}
+
+        void FixedUpdate() override {}
+        void Update() override {}
+        void LateUpdate() override {}
+
+        void OnDisable() override {}
+        void OnDestroy() override {}
+        
+    private:
+        float m_LifeTime;
+    };
+}
