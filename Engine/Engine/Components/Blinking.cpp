b42af21b4ee017f4d4cd16d78c8937576bfeae0f
Dylan Burgisser <33450259+DijiOfficial@users.noreply.github.com>
2026-06-22 21:52:38 +0200

    [ADD] blinking comp

---
+++ Engine/Engine/Components/Blinking.cpp
@@ -0,0 +1,19 @@
+﻿#include "Blinking.h"
+#include "Render.h"
+#include "../Core/GameObject.h"
+#include "../Singleton/TimeSingleton.h"
+
+void diji::Blinking::Init()
+{
+    m_RenderCompPtr = GetOwner()->GetComponent<Render>();
+}
+
+void diji::Blinking::Update()
+{
+    m_ElapsedTime -= TimeSingleton::GetInstance().GetDeltaTime();
+    if (m_ElapsedTime >= 0)
+        return;
+    
+    m_ElapsedTime = m_RenderCompPtr->IsRendering() ? m_OnScreenTime : m_OffScreenTime;
+    m_RenderCompPtr->ToggleRendering();
+}
---
+++ Engine/Engine/Components/Blinking.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "../Components/Component.h"
+
+namespace diji
+{
+    class Render;
+
+    class Blinking final : public Component
+    {
+    public:
+        explicit Blinking(GameObject* ownerPtr) : Component{ ownerPtr } {}
+        explicit Blinking(GameObject* ownerPtr, const float offScreenTime, const float onScreenTime) : Component{ ownerPtr }, m_OffScreenTime{ offScreenTime }, m_OnScreenTime{ onScreenTime } {}
+        ~Blinking() noexcept override = default;
+
+        void Init() override;
+        void OnEnable() override {}
+        void Start() override {}
+
+        void FixedUpdate() override {}
+        void Update() override;
+        void LateUpdate() override {}
+
+        void OnDisable() override {}
+        void OnDestroy() override {}
+        void SetOffScreenTime(const float time) { m_OffScreenTime = time; }
+        void SetOnScreenTime(const float time) { m_OnScreenTime = time; }
+
+    private:
+        Render* m_RenderCompPtr = nullptr;
+        float m_OffScreenTime = 0.5f;
+        float m_OnScreenTime = 0.5f;
+        float m_ElapsedTime = 0.0f;
+    };
+}
